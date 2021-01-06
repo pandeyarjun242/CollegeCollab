95a096939f691da2f1b698cfa72a7e6209a8ed9b
pandeyarjun242 <pandeyarjun.242@gmail.com>
2021-01-06 22:43:59 +0530

    hello

--- CommonChecklist/settings.py
+++ CommonChecklist/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -139,6 +140,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'CommonChecklist/static/')
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn CommonChecklist.wsgi
